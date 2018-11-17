ab1908c1de50350a0dd20c74b3c6ac7834b659b7
Konstantin Skydan <kskydan@gmail.com>
2018-11-17 16:02:19 +0200

    added RU localization

---
+++ Intl/localizationData/ru.js
@@ -0,0 +1,39 @@
+export default {
+  locale: 'ru',
+  messages: {
+    siteTitle: 'MERN Блог',
+    addPost: 'Добавить статью',
+    switchLanguage: 'Переключить язык',
+    twitterMessage: 'Мы в Twitter',
+    by: 'Автор: ',
+    deletePost: 'Удалить статью',
+    createNewPost: 'Создать новую статью',
+    authorName: 'Имя автора',
+    postTitle: 'Название статьи',
+    postContent: 'Текст статьи',
+    submit: 'Отправить',
+    comment: `user {name} {value, plural,
+    	  =0 {does not have any comments}
+    	  =1 {has # comment}
+    	  other {has # comments}
+    	}`,
+    HTMLComment: `user <b style='font-weight: bold'>{name} </b> {value, plural,
+    	  =0 {does not have <i style='font-style: italic'>any</i> comments}
+    	  =1 {has <i style='font-style: italic'>#</i> comment}
+    	  other {has <i style='font-style: italic'>#</i> comments}
+    	}`,
+    nestedDateComment: `user {name} {value, plural,
+    	  =0 {does not have any comments}
+    	  =1 {has # comment}
+    	  other {has # comments}
+    	} as of {date}`,
+    createComment: 'Добавить новый комментарий',
+    commentContent: 'Текст комментария',
+    authorComment: 'Автор:',
+    btnEditComment: 'Редактировать',
+    btnDeleteComment: 'Удалить',
+    btnCreateComment: 'Добавить',
+    btnSaveComment: 'Сохранить',
+    btnCancelComment: 'Отмена',
+  },
+};
